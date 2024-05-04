81d68700cf23cea36e77693dfb937b17210f72a3
Agustin Legizamon <agustin.leg7@gmail.com>
2024-05-04 01:18:27 -0300

    refactor(auth): update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,36 @@
-export { auth as middleware } from "@/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+import { type Session } from "next-auth";
+
+import { auth } from "@/lib/auth";
+
+interface NextAuthRequest extends NextRequest {
+  auth: Session | null;
+}
+
+export default auth((req: NextAuthRequest) => {
+  const isAuth = !!req.auth?.user;
+  const isAuthPage =
+    req.nextUrl.pathname.startsWith("/login") ||
+    req.nextUrl.pathname.startsWith("/register");
+
+  if (isAuthPage) {
+    if (isAuth) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    return;
+  }
+
+  if (!isAuth) {
+    let from = req.nextUrl.pathname;
+    if (req.nextUrl.search) {
+      from += req.nextUrl.search;
+    }
+    return NextResponse.redirect(
+      new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
+    );
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
